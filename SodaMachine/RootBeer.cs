a0caebc2a1e0f92a9ef2d0775ffe4999fb03ccba
unknown <ana.nalbandyan@gmail.com>
2021-02-16 13:31:19 -0800

    Init

---
+++ SodaMachine/RootBeer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class RootBeer:Can
+    {
+        //Member Variables (Has A)
+      //protected double price; 
+      //public string name;
+
+        //Constructor (Spawner)
+        public RootBeer()
+        {
+            Name = "Root Beer";
+            price = 0.30;
+        }
+        //Member Methods (Can Do)
+    }
+}
